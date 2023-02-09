a4d78687c68656b5ddba413ca35190afda817334
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-09 18:59:13 +0000

    Prototipo 1 generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateexcuse();
+  });
+};
+
+let generateexcuse = () => {
+  let who = [
+    "Bob Esponja",
+    "Mi trauma de la infancia",
+    "La tortuga de Juan",
+    "Jovany Vazquez"
+  ];
+  let what = ["se comió", "lloró", "voló y me hizo volar", "saltó"];
+  let when = [
+    "una milanesa con papas fritas",
+    "justo a tiempo",
+    "y pensé no eres tu soy yo, que te he cogido asco",
+    "cuando eran 3 motoristas que eran motos",
+    "mientras jugaba acá en la arbolada"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
 };
